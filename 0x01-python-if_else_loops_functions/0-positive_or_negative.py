5944334a1a396a0cc250237136901f4da9c6c0f2
samuel-george <georgeakonjom003@gmail.com>
2023-06-06 23:49:26 +0200

    fixed issue where zero does not print

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number:
-    if number > 0:
-        print(f"{number:d} is positive")
-    if number < 0:
-        print(f"{number:d} is negative")
+if number is not None:
     if number == 0:
-        print(f"{number:d} is zero")
+        print(f"{number} is zero")
+    elif number < 0:
+        print(f"{number:d} is negative")
+    elif number > 0:
+        print(f"{number:d} is positive")
