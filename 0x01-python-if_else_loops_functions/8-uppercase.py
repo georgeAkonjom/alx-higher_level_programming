dd1a26d415e1fdfd871ea972559a4638671a1ca3
samuel-george <georgeakonjom003@gmail.com>
2023-06-07 00:48:11 +0100

    task 8. fixed issue where checker flags for less than two string format print statements

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(s):
+    for c in s:
+        ascii_val = ord(c)
+        if 97 <= ascii_val <= 122:
+            ascii_val -= 32
+        print("{:c}".format(ascii_val), end="")
+    print("", end="\n")
