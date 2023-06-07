fec3dbe7624d003db28408e51e91206f7091c563
samuel-george <georgeakonjom003@gmail.com>
2023-06-07 02:44:54 +0100

    fixed error where floating point division caused discrepancies in results against checker task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if a is not None and b is not None:
-        result = 1
-        if a == 0:
-            result = 0
-            return result
-        if b == 0:
-            return result
-        if b < 0:
-            for i in range(abs(b)):
-                result = result // a
-            return result
-        else:
-            for i in range(abs(b)):
-                result = result * a
-            return result
-    return result
+    result = 1
+    if a == 0:
+        result = 0
+        return result
+    if b == 0:
+        return result
+    if b < 0:
+        for i in range(abs(b)):
+            result = result / a
+        return result
+    else:
+        for i in range(abs(b)):
+            result = result * a
+        return result
