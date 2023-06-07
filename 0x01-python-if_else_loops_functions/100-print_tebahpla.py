1655f48435abace37194ed89426d07f2bea717b8
samuel-george <georgeakonjom003@gmail.com>
2023-06-07 03:48:58 +0100

    removed test files, cleanup

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') + 1):
+for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        i -= 32
-    print("{:c}".format(i))
-
+        i = chr(i)
+        print(i)
+    else:
+        i = chr(i)
+        print(i.upper())
--- 0x01-python-if_else_loops_functions/test100.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        i = chr(i)
-        print(i)
-    else:
-        i = chr(i)
-        print(i.upper())
