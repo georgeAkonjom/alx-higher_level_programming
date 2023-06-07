fbffbe83e080f81bb8571a3b1069edf4598fcc04
samuel-george <georgeakonjom003@gmail.com>
2023-06-07 02:22:32 +0100

    task 11 fixed error where calculations with negative numbers produced incorrect results

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if a is not None or b is not None:
+        result = 1
+        if a == 0:
+            result = 0
+            return result
+        if b == 0:
+            return result
+        if b < 0:
+            for i in range(abs(b)):
+                result = result / a
+                return result
+        else:
+            for i in range(abs(b)):
+                result = result * a
+    return result
