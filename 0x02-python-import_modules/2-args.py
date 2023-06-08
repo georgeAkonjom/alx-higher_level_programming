9952df8ed58983ccd9050ec1f1ede1d09751585f
samuel-george <georgeakonjom003@gmail.com>
2023-06-08 19:27:06 +0100

    tasks 0 through 2.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from sys import argv
+if __name__ == "__main__":
+    arg_no = len(argv) - 1
+    output = ""
+    if arg_no:
+        output = f"{arg_no} arguments:"
+    if arg_no == 1:
+        output = f"{arg_no} argument:"
+    if arg_no == 0:
+        output = f"{arg_no} arguments."
+    print("{}".format(output))
+    for i in range(arg_no + 1):
+        arg = argv[i]
+        if i == 0:
+            continue
+        else:
+            print("{}: {}".format(i, arg))
+        
