ba1c1ced76df69e6527349084ad69b9c1399a76a
samuel-george <georgeakonjom003@gmail.com>
2023-06-08 23:01:14 +0100

    troubleshooting requirement 3 for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
     arg_no = len(argv) - 1
     output = ""
-    if arg_no:
-        output = f"{arg_no} arguments:"
-    if arg_no == 1:
-        output = f"{arg_no} argument:"
+
     if arg_no == 0:
-        output = f"{arg_no} arguments."
-    print("{}".format(output))
-    for i in range(arg_no + 1):
+        output = f"{arg_no} argument{'s' if arg_no != 1 else ''}."
+    else:
+        output = f"{arg_no} argument{'s' if arg_no != 1 else ''}:"
+
+    print(output)
+
+    for i in range(1, len(argv)):
         arg = argv[i]
-        if i == 0:
-            continue
-        else:
-            print("{}: {}".format(i, arg))        
+        print(f"{i}: {arg}")
