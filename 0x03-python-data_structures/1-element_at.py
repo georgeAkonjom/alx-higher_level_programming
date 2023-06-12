c91a07ce291cb1f4fdc91645fa1ac3ce8eee0874
samuel-george <georgeakonjom003@gmail.com>
2023-06-12 16:26:52 +0100

    Fixed issue where checking for negative number causes list inclusivity check to fail

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in range(len(my_list)):
-        if idx > i:
-            return(None)
     if idx < 0:
+        return(None)
+    length = len(my_list) - 1
+    if idx > length:
         return(None)
     if my_list[idx]:
         athere = my_list[idx]
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -2,5 +2,6 @@
 element_at = __import__('1-element_at').element_at
 
 my_list = [1, 2, 3, 4, 5]
-idx = -2000
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+for i in range(-3, 20):
+    idx = i
+    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
