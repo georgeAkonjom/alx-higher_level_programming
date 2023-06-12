1d65264af62b492aa3b8504d177d99ae89d7e5c3
samuel-george <georgeakonjom003@gmail.com>
2023-06-12 16:02:55 +0100

    fixed issue inserting an elem at a range that is  the existing list return the wrong error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list):
-        return(None)
+    for i in range(len(my_list)):
+        if idx > i:
+            return(None)
     if idx < 0:
         return(None)
     if my_list[idx]:
