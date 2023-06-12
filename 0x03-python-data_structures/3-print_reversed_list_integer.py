70dff7cda81eb3002142f75df020ffdf1138b357
samuel-george <georgeakonjom003@gmail.com>
2023-06-12 23:33:17 +0100

    troubleshooting task 8. No idea what the issue is.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = (len(my_list))
-    if length:
-        for i in range(length):
-            length -= 1
-            print("{:d}".format(my_list[length]))
+    if my_list:
+        index = len(my_list) - 1
+        while index >= 0:
+            print("{:d}".format(my_list[index]))
+            index -= 1
+
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-no_c = __import__('5-no_c').no_c
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
