5833f3a0386e841740b3f1537415ea32f5e45ff5
samuel-george <georgeakonjom003@gmail.com>
2023-06-14 03:58:48 +0100

    fixed bubble sort logic: task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    length = len(my_list) - 1
-    if my_list:
-        while length > 1:
-            j = 0
-            while j < length:
-                if my_list[j] > my_list[j + 1]:
-                    buffer = my_list[j]
-                    my_list[j] = my_list[j + 1]
-                    my_list[j] = buffer
-                j += 1
-            length -= 1
-        return(my_list[length])
-    else:
-        return(None)
+    i = len(my_list) - 1
+    while i > 1:
+        j = 0
+        while j < i:
+            if my_list[j] > my_list[j + 1]:
+                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+            j += 1
+        i -= 1
+    return(my_list)
+
