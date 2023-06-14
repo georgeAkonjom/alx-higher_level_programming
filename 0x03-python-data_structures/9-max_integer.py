e7c6c3fd7ac2c30b8cc8a486c6f864ee5cf86c13
samuel-george <georgeakonjom003@gmail.com>
2023-06-14 02:54:48 +0100

    task 10. fixed hopefully issue where none isnt printed when list is empty: task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     length = len(my_list) - 1
-
-    while length > 1:
-        j = 0
-        while j < length:
-            if my_list[j] > my_list[j + 1]:
-                buffer = my_list[j]
-                my_list[j] = my_list[j + 1]
-                my_list[j] = buffer
-            j += 1
-        length -= 1
-    return(my_list[length])
+    if my_list:
+        while length > 1:
+            j = 0
+            while j < length:
+                if my_list[j] > my_list[j + 1]:
+                    buffer = my_list[j]
+                    my_list[j] = my_list[j + 1]
+                    my_list[j] = buffer
+                j += 1
+            length -= 1
+        return(my_list[length])
+    else:
+        return(None)
