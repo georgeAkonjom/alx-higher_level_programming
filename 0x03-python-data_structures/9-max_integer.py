e844cde2b9032b34fcf085ca810acc03ac648530
samuel-george <georgeakonjom003@gmail.com>
2023-06-14 04:04:06 +0100

    fixed the return value on the max int func: task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    i = len(my_list) - 1
-    while i > 1:
-        j = 0
-        while j < i:
-            if my_list[j] > my_list[j + 1]:
-                my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
-            j += 1
-        i -= 1
-    return(my_list)
-
+    if my_list:
+        i = len(my_list) - 1
+        while i > 1:
+            j = 0
+            while j < i:
+                if my_list[j] > my_list[j + 1]:
+                    my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+                j += 1
+            i -= 1
+        return(my_list[len(my_list) - 1])
+    else:
+        return(None)
