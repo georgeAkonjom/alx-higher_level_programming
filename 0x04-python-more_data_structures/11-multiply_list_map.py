6145a31802d15d6a22537b7d4078f4541c4abaf6
samuel-george <georgeakonjom003@gmail.com>
2023-06-15 01:55:22 +0100

    i had for lines instead of three... going to sleep now

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    output = map(lambda x: x * number, my_list)
-    return list(output)
+    return(list(map(lambda x: x * number, my_list)))
