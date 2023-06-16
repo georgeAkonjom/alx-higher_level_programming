3ae3cef733843e77572a4d50bc2575558013fda8
samuel-george <georgeakonjom003@gmail.com>
2023-06-17 00:01:46 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_num = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    index = 0
+    result = 0
+    for letter in range(len(roman_string)):
+        key = roman_string[letter]
+        if len(roman_string) == 1:
+            result += roman_num[roman_string[letter]]
+            return result
+        if index == 0:
+            # print("val of first key: {}".format(roman_num[key]))
+            if roman_num[roman_string[letter]] < roman_num[roman_string[letter + 1]]:
+                result -= roman_num[key]
+                print
+                # print('first key is lesser than second')
+                index += 1
+                continue
+        index += 1
+        result += roman_num[key]
+        # print(roman_num[key])
+    # print('result: {}'.format(result))
+    return(result)
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-simple_delete = __import__('8-simple_delete').simple_delete
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+""" Roman to Integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
-new_dict = simple_delete(a_dictionary, 'track')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-print("--")
-print("--")
-new_dict = simple_delete(a_dictionary, 'c_is_fun')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
