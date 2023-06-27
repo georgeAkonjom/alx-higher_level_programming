d0ae0ba1c244868870793465147f151af98e5c6b
samuel-george <georgeakonjom003@gmail.com>
2023-06-28 00:18:56 +0100

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    final = 0
+    for elem in range(x):
+        try:
+            print("{:d}".format(my_list[elem]), end="")
+            final += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            print()
+            return(final)
+    print()
+    return(final)
