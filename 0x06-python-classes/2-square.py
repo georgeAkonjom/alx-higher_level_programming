6f93b5a18dc7b173d2040797225fd00395125263
samuel-george <georgeakonjom003@gmail.com>
2023-07-03 00:55:42 +0100

    this is a thing i guess

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Square module. With a size to it this time"""
+"""Square module. With a private instance of size, Type and Value gaurded"""
 
 
 class Square:
     """The square class"""
     def __init__(self, size=0):
         """Initialize the objects attributes"""
-        self.__size = size
-
-    @size.setter
-    def size(self, val):
-        try:
-            if val.isdigit():
-                self.__size = val
-        except(ValueError):
-            print("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
