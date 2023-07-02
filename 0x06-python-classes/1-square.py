bf03395e7f3af045d27304229d421f9ce81ab84f
samuel-george <georgeakonjom003@gmail.com>
2023-07-03 00:15:27 +0100

    a thing

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Square module. With a size to it this time
-"""
+"""Square module. With a size to it this time"""
 
 
 class Square:
-    """The square class
-    """
+    """The square class"""
     self.__size = size
