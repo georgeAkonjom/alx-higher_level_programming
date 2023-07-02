3aff84ca3695ae05afb3eaf8d5fbad07df94ed76
samuel-george <georgeakonjom003@gmail.com>
2023-07-03 00:10:03 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square module. With a size to it this time"""
+
+
+class Square:
+    """The square class"""
+    def __init__(self, size):
+        """init self and size"""
+        @property
+        def size(self):            
+            return self.__size
