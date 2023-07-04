01c18fb4245dfca0160dec689de2864b3c759c3a
samuel-george <georgeakonjom003@gmail.com>
2023-07-04 23:11:06 +0100

    task 6 initial commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Square module. With a private instance of size, Type and Value gaurded"""
+
+
+class Square:
+    """The square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the objects attributes"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not len(position) == 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calcs and returns the area of a square object"""
+        return pow(self.size, 2)
+
+    def my_print(self):
+        """prints the square using self.size as a metric"""
+        i = 0
+        if self.size == 0:
+            print()
+        for i in range(self.position[1]):
+            print()
+        for j in range(self.size):
+            print("{}{}".format(" " * self.position[0], '#'*self.size))
